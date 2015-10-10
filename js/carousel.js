249562973c7823e5d54f3dfcacbbf29d79cee6f0
leonchike <chike@kivvik.com>
2015-10-10 11:24:19 -0400

    new test site

--- js/carousel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#ourUpdatesContent li a').click(function(e) {
-	    $(this).closest('ul').find('.selectActive').removeClass('selectActive');
-	    $(this).parent('li').addClass('selectActive');
-
-	    $('#' + this.href.split('#')[1]).fadeIn('slow').siblings('.changingTextIconDiv').hide();
-	    return false;
-	});
-});
